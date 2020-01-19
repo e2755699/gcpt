9f29d572088eed39560bc5c52d4402bb16dd2430
Maxwell Chen <Phelix.tw@gmail.com>
2020-01-20 01:43:32 +0800

    Add project files.

---
+++ gcpt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gcpt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
